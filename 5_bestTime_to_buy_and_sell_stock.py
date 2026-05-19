706eacd55a0b89b9888021d6cfaea57e8b1a2194
natehc <chetanchetu4940@gmail.com>
2026-05-19 16:09:41 +0530

    Added comments in order to understand the problem thoroughly

--- 5_bestTime_to_buy_and_sell_stock.py
+++ 5_bestTime_to_buy_and_sell_stock.py
@@ -18,20 +18,21 @@ s = Solution()
 print("Maximum Profit is:", s.max_profit(prices))
 
 
+# //------------------------------------------------//
 # Optimal solution
 class SolutionOptimized:
     def max_profit(self, prices: list[int]) -> int:
-        min_price = prices[0]
-        max_profit = 0
+        min_price = prices[0]  # Initialise min_price with first element
+        max_profit = 0  # Initialise max_profit with 0
 
         for price in prices:
-            if price < min_price:
-                min_price = price
+            if price < min_price:  # check if current price is less than min_price
+                min_price = price  # update min_price
 
             profit = price - min_price
 
-            if profit > max_profit:
-                max_profit = profit
+            if profit > max_profit:  # check if profit is greater than max_profit
+                max_profit = profit  # update max_profit
 
         return max_profit
 
