f6ce1b4b54ff98992ca741c34992b7c74f41ee02
natehc <chetanchetu4940@gmail.com>
2026-05-19 16:07:29 +0530

    Added the Optimal Solution as well

--- 5_bestTime_to_buy_and_sell_stock.py
+++ 5_bestTime_to_buy_and_sell_stock.py
@@ -1,17 +1,41 @@
+# Brute Force approach
 class Solution:
     def max_profit(self, prices: list[int]) -> int:
 
         max_profit = 0
         for buy_day in range(len(prices)):
             for sell_day in range(buy_day + 1, len(prices)):
-                profit = prices[sell_day] - prices[buy_day]
+                profit = prices[sell_day] - prices[buy_day]  # calculate profit
 
                 if profit > max_profit:
-                    max_profit = profit
+                    max_profit = profit  # update max_profit
 
         return max_profit
 
 
 prices = list(map(int, input("Enter the Stock prices:").split()))
 s = Solution()
+print("Maximum Profit is:", s.max_profit(prices))
+
+
+# Optimal solution
+class SolutionOptimized:
+    def max_profit(self, prices: list[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+
+prices = list(map(int, input("Enter the Stock prices:").split()))
+s = SolutionOptimized()
 print("Maximum Profit is:", s.max_profit(prices))
