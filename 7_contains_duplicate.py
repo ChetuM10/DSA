617ca815e33c798838182923695d1f05dca26a2a
natehc <chetanchetu4940@gmail.com>
2026-05-30 17:49:50 +0530

    solved for the optimal sol (LC - 217)

--- 7_contains_duplicate.py
+++ 7_contains_duplicate.py
@@ -1,14 +1,33 @@
-# Brute Force
+# # Brute Force
+# def containDuplicate(nums):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] == nums[j]:
+#                 return True
+#     return False
+
+
+# nums = [1, 2, 3, 4, 1]
+# dup = containDuplicate(nums)
+# print("----------------------")
+# print("Contains Duplicates?:", dup)
+# print("----------------------")
+
+# Optimal Solution
+
+
 def containDuplicate(nums):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] == nums[j]:
-                return True
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
     return False
 
 
-nums = [1, 2, 3, 4, 1]
+nums = [1, 2, 3, 4, 5]
 dup = containDuplicate(nums)
-print("----------------------")
-print("Contains Duplicates?:", dup)
-print("----------------------")
+
+print("------------------------")
+print("Contains Dupicate?:", dup)
+print("------------------------")
