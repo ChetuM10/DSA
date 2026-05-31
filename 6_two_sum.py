a16c1edc5c68e710bbbb6844c7ae7d47dac333ae
natehc <chetanchetu4940@gmail.com>
2026-05-31 20:39:08 +0530

    added brute force for two_sum

--- 6_two_sum.py
+++ 6_two_sum.py
@@ -1,13 +1,24 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i in range (len(nums)):
-            needed = target - nums[i] # 9 - 2 = 7
-            if needed in seen: #checks if the needed value is present in the seen dictionary
-                return [seen[needed], i] #return the index of the needed value and the current index
-            seen[nums[i]] = i #add the current value and index to the seen dictionary
+# class Solution:
+#     def twoSum(self, nums: list[int], target: int) -> list[int]:
+#         seen = {}
+#         for i in range (len(nums)):
+#             needed = target - nums[i] # 9 - 2 = 7
+#             if needed in seen: #checks if the needed value is present in the seen dictionary
+#                 return [seen[needed], i] #return the index of the needed value and the current index
+#             seen[nums[i]] = i #add the current value and index to the seen dictionary
 
 
-s = Solution()
+# s = Solution()
+# print("Target:", 9)
+# print("indexes:", s.twoSum([2,7,11,15], 9))
+
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
 print("Target:", 9)
-print("indexes:", s.twoSum([2,7,11,15], 9))
+print("indexes:", twoSum([11, 15, 2, 7], 9))
